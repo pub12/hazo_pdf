924142a6fa8368872f2d6a97bd38e4972fe01ef6
Pubs <pubudu79@gmail.com>
2026-01-30 10:26:36 +1100

    fixe

--- app/lib/hazo_pdf.ts
+++ app/lib/hazo_pdf.ts
@@ -3,4 +3,4 @@
  * This file allows the test app to import from '@/app/lib/hazo_pdf'
  * which resolves to the built dist/ folder
  */
-export * from '../../dist/index.js';
+export * from '@/dist/index.js';
--- app/lib/hazo_pdf_server.ts
+++ app/lib/hazo_pdf_server.ts
@@ -3,4 +3,4 @@
  * This file allows the test app to import from '@/app/lib/hazo_pdf_server'
  * which resolves to the built dist/server folder
  */
-export * from '../../dist/server/index.js';
+export * from '@/dist/server/index.js';
